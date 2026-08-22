baa4cfe9cae6b2c6fbe01102494b665b70601e99
Mohd Kaif Ansari <155880242+techykaif@users.noreply.github.com>
2026-08-22 21:36:26 +0530

    fix: scope robots to discovery crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -17,10 +17,6 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: "OAI-AdsBot",
         allow: "/",
       },
-      {
-        userAgent: "GPTBot",
-        allow: "/",
-      },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
